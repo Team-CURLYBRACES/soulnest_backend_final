6bac2a615aada514bab28c6004aed21ba522c3d4
amethaaa <amethajayawardena@gmail.com>
2024-03-30 00:21:20 +0530

    change url

--- automated_processes/views.py
+++ automated_processes/views.py
@@ -4,7 +4,7 @@ import requests
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-url = 'http://188.166.196.163:8001/predict/'
+url = 'https://9a76-2402-d000-a400-3124-2c1a-67c4-4303-9cff.ngrok-free.app/predict'
 
 
 @csrf_exempt
@@ -14,13 +14,17 @@ def index(request):
         if user.chats:
             messages = ""
         for i in range(len(user.chats)):
-            if user.chats[i].is_stress_checked == True:
+            if user.chats[i].is_stress_checked == False:
                     messages += (user.chats[i].text + '.')
                     user.chats[i].is_stress_checked == True
                     data = {"response":messages}
         response = requests.post(url, json={"response":messages})
+        print(response)
+        print(messages)
         data = response.json()
+        print(data)
         percentage = data['stress_percentage']
+        print('here')
         user.stress_level.append(percentage)
         user.save()
     return JsonResponse({"message":"Process carried out successfully"}, status=201)
